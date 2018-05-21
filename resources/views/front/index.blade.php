3a0ed32ce348f4e77f4d1d2c272f7dbdf58b3ba5
arnaumacaya <arnaumacaya@gmail.com>
2018-05-21 17:24:54 +0200

    landing pade little changes

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -65,15 +65,8 @@
                 <div class="row">
                     <div class="col-lg-8 mx-auto">
                         <h2>About Armagedon</h2>
-                        <p>Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-                            <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.
-                        </p>
-                        <p>This theme features stock photos by
-                            <a href="http://gratisography.com/">Gratisography</a>
-                            along with a custom Google Maps skin courtesy of
-                            <a href="http://snazzymaps.com/">Snazzy Maps</a>.
-                        </p>
-                        <p>Grayscale includes full HTML, CSS, and custom JavaScript files along with SASS and LESS files for easy customization!</p>
+                        <p>This application developed by Armagedon SA allows you to monitor your computer or server stats like CPU, RAM or Network easly before you are logged and validated.</p>
+                        <p>Also this aplication allows you to know historic average of computer/server rendiment.</p>
                     </div>
                 </div>
             </div>
@@ -84,7 +77,7 @@
                 <div class="col-lg-8 mx-auto">
                     <h2>Download</h2>
                     <p>You can download Armagedon in the following link.</p>
-                    <a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">Visit Download Page</a>
+                    <a href="https://github.com/cristianfonolla/armagedonfrontend" class="btn btn-default btn-lg" target="_blank">Visit Download Page</a>
                 </div>
             </div>
         </section>
